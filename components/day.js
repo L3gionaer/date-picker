74dd13901c811fe0ec2ef84374def9688521f00f
L3gionaer <patrick@waidmann.email>
2022-11-05 20:04:34 +0100

    Changed Day Component (date instead of dayCount)

--- components/day.js
+++ components/day.js
@@ -3,66 +3,78 @@ import EventEmitter from "../event-emitter.js";
 // is Current Date? Auslagern
 
 class Day extends EventEmitter {
-  constructor({dayCount, isCurrentDate}) {
+  #component;
+  #isCurrentDate;
+  #date;
+
+  constructor({date, isCurrentDate}) {
     super();
 
-    this.component;
-    this.isCurrentDate = isCurrentDate;
-    this.dayCount = dayCount;
+    this.#component;
+    this.#isCurrentDate = isCurrentDate;
+    this.#date = date;
 
-    this._init();
+    this.#init();
   }
 
-  _init() {
-    this._render();
+  get component() {
+    return this.#component;
+  }
+
+  get date() {
+    return this.#date;
+  }
+
+  #init() {
+    this.#render();
   }
 
   disable() {
-    this.component.style.color = "grey"
+    this.#component.style.color = "grey"
   }
 
   select() {
-    this.component.style.backgroundColor = "red";
+    this.#component.style.backgroundColor = "red";
   }
 
   selectBetween() {
-    this.component.style.backgroundColor = "yellow";
+    this.#component.style.backgroundColor = "yellow";
   }
 
   deselect() {
-    this.component.style.backgroundColor = "transparent";
+    this.#component.style.backgroundColor = "transparent";
   }
 
   highlight() {
-    this.component.style.outline = "1px solid red"
+    this.#component.style.outline = "1px solid red"
   }
 
   removeHighlight() {
-    this.component.style.outline = "none"
+    this.#component.style.outline = "none"
   }
 
   currentDate() {
-    this.component.style.backgroundColor = "blue";
+    this.#component.style.backgroundColor = "blue";
   }
 
-  _render() {
-    this.component = document.createElement("td");
-    this.component.setAttribute("data-day", this.dayCount);
-    this.component.textContent = this.dayCount;
+  #render() {
+    this.#component = document.createElement("td");
+    this.#component.setAttribute("data-day", this.#date);
+    this.#component.textContent = this.#date;
 
-    if(this.isCurrentDate) {
+    if(this.#isCurrentDate) {
       this.currentDate();
     }
 
-    this.component.addEventListener("click", e => {
+    this.#component.addEventListener("click", e => {
       this.emit("click", this);
     });
 
-    this.component.addEventListener("mouseover", e => {
+    this.#component.addEventListener("mouseover", e => {
       this.emit("mouseover", this);
     });
 
-    this.component.addEventListener("mouseleave", e => {
+    this.#component.addEventListener("mouseleave", e => {
       this.emit("mouseleave", this);
     })
   }
